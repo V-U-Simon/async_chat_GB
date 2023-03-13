e1833b93a4c676a62a4e3a6abe10af14d1c385c4
V-U-Simon <V-U-Simon@yandex.ru>
2023-03-13 08:21:30 +0300

    handle closing connecntion

--- lesson_03/client_side.py
+++ lesson_03/client_side.py
@@ -2,12 +2,9 @@ from socket import socket, AF_INET, SOCK_STREAM
 from vars import server_address
 from loguru import logger
 
+with socket(AF_INET, SOCK_STREAM) as s:
+    s.connect(server_address)
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect(server_address)
-
-s.send(b"test message")
-data = s.recv(1024)
-logger.debug(data.decode("utf-8"))
-
-s.close()
+    s.send(b"test message")
+    data = s.recv(1024)
+    logger.debug(data.decode("utf-8"))
--- lesson_03/server_side.py
+++ lesson_03/server_side.py
@@ -2,17 +2,17 @@ from socket import socket, AF_INET, SOCK_STREAM
 from vars import server_address
 from loguru import logger
 
+with socket(AF_INET, SOCK_STREAM) as s:
+    s.bind(server_address)
+    s.listen(5)
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind(server_address)
-s.listen(5)
+    while True:
+        s_connect, client_address = s.accept()
+        logger.debug(f"Client connected from {client_address}")
 
-while True:
-    s_connect, client_address = s.accept()
-    logger.debug(f"Client connected from {client_address}")
-
-    data = s_connect.recv(1024)
-    logger.debug(f"Received data: {data}")
-
-    s_connect.send(b"message form server: get your message")
-    s_connect.close()
+        try:
+            data = s_connect.recv(1024)
+            logger.debug(f"Received data: {data}")
+            s_connect.send(b"message form server: get your message")
+        finally:
+            s_connect.close()
