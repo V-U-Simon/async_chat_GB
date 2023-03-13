7df772087905bd5bee681ee095aa5e7269c99c5d
V-U-Simon <V-U-Simon@yandex.ru>
2023-03-13 08:11:49 +0300

    fix mistyping

--- lesson_03/client_side.py
+++ lesson_03/client_side.py
@@ -1,11 +1,13 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from vars import server_address
+from loguru import logger
+
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(server_address)
 
 s.send(b"test message")
 data = s.recv(1024)
-print(data.de("utf-8"))
+logger.debug(data.decode("utf-8"))
 
 s.close()
