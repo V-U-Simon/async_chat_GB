e2943d7b547475b3523f8246f74be3990af858b5
V-U-Simon <V-U-Simon@yandex.ru>
2023-03-13 10:44:19 +0300

    add argparse to server-side

--- lesson_03/server_side.py
+++ lesson_03/server_side.py
@@ -4,17 +4,32 @@ from loguru import logger
 import argparse
 
 
-with socket(AF_INET, SOCK_STREAM) as s:
-    s.bind((DEFAULT_IP, DEFAULT_PORT))
-    s.listen(5)
+def main():
+    parser = argparse.ArgumentParser(description='run server')
+    parser.add_argument('--address', '-a', type=str, default=DEFAULT_IP, help='set server address (default: 127.0.0.1)')
+    parser.add_argument('--port', '-p', type=int, default=DEFAULT_PORT, help='set server port (default: 8880)')
+    args = parser.parse_args()
 
-    while True:
-        s_connect, client_address = s.accept()
-        logger.debug(f"Client connected from {client_address}")
+    run_server(args.address, args.port)
 
-        try:
-            data = s_connect.recv(1024)
-            logger.debug(f"Received data: {data}")
-            s_connect.send(b"message form server: get your message")
-        finally:
-            s_connect.close()
+
+def run_server(address, port):
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.bind((address, port))
+        logger.debug(f'start server on {address}:{port}')
+        s.listen(5)
+
+        while True:
+            s_connect, client_address = s.accept()
+            logger.debug(f"Client connected from {client_address}")
+
+            try:
+                data = s_connect.recv(1024)
+                logger.debug(f"Received data: {data}")
+                s_connect.send(b"message form server: get your message")
+            finally:
+                s_connect.close()
+
+
+if __name__ == "__main__":
+    main()
