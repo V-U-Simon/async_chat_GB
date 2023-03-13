d3a45915dff1a508aec37207601be8af4a2cae61
V-U-Simon <V-U-Simon@yandex.ru>
2023-03-13 11:07:11 +0300

    add argparse to client-side

--- lesson_03/client_side.py
+++ lesson_03/client_side.py
@@ -1,10 +1,27 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from conf import DEFAULT_IP, DEFAULT_PORT
 from loguru import logger
+import argparse
 
-with socket(AF_INET, SOCK_STREAM) as s:
-    s.connect((DEFAULT_IP, DEFAULT_PORT))
 
-    s.send(b"test message")
-    data = s.recv(1024)
-    logger.debug(data.decode("utf-8"))
+def main():
+    parser = argparse.ArgumentParser(description='run client')
+    parser.add_argument('--address', '-a', type=str, default=DEFAULT_IP, help='sent to server address (default: 127.0.0.1)')
+    parser.add_argument('--port', '-p', type=str, default=DEFAULT_PORT, help='set to server port (default: 8893)')
+    args = parser.parse_args()
+
+    run_client(args.address, args.port)
+
+
+def run_client(address, port):
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.connect((DEFAULT_IP, DEFAULT_PORT))
+        logger.debug(f'connected to server ({DEFAULT_IP}:{DEFAULT_PORT})')
+
+        s.send(b"test message")
+        data = s.recv(1024)
+        logger.debug(data.decode("utf-8"))
+
+
+if __name__ == '__main__':
+    main()
--- lesson_03/server_side.py
+++ lesson_03/server_side.py
@@ -7,7 +7,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='run server')
     parser.add_argument('--address', '-a', type=str, default=DEFAULT_IP, help='set server address (default: 127.0.0.1)')
-    parser.add_argument('--port', '-p', type=int, default=DEFAULT_PORT, help='set server port (default: 8880)')
+    parser.add_argument('--port', '-p', type=int, default=DEFAULT_PORT, help='set server port (default: 8893)')
     args = parser.parse_args()
 
     run_server(args.address, args.port)
@@ -28,7 +28,7 @@ def run_server(address, port):
                 logger.debug(f"Received data: {data}")
                 s_connect.send(b"message form server: get your message")
             finally:
-                s_connect.close()
+                s_connect.close
 
 
 if __name__ == "__main__":
